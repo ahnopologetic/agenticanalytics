fcb9f28d7c1096c76644ad09d0c6a522faa1743c
ahnopologetic <stahn1995@gmail.com>
2025-05-31 11:54:21 -0400

    feat: be expressive on git manager agent

--- server/agents/sub_agents/repo_reader/sub_agents/git_manager/agent.py
+++ server/agents/sub_agents/repo_reader/sub_agents/git_manager/agent.py
@@ -1,20 +1,18 @@
 from google.adk.agents import LlmAgent
 
-from utils.github import aclone_repository, apull_repository, aswitch_branch
+from utils.github import aclone_repository
 
 git_manager_agent = LlmAgent(
     name="git_manager_agent",
     model="gemini-2.0-flash",
     description="Manage the git repository.",
     instruction="""
-    Given the repository url, clone the repository and stay up to date with the latest changes.
-
-    You have the following tools:
-    - aclone_repository(repo_name: str, branch: str = "main"): Clone the repository.
-
-    If you are given a url, you need to extract the repository name and branch from the url.
-
-    If the repository is not cloned, clone the repository.
+    Given the repository url, clone the repository using the aclone_repository tool.
+    
+    Example
+    User Input: https://github.com/UnlyEd/next-right-now/tree/master
+    Function Call: aclone_repository(repo_name="UnlyEd/next-right-now", branch="master")
+    Reason: The repository name is the part of the URL that identifies the repository, e.g., "facebook/react" or "UnlyEd/next-right-now". From the given URL, the repository name is "UnlyEd/next-right-now". Also, the branch is master.
     """,
     tools=[aclone_repository],
 )
