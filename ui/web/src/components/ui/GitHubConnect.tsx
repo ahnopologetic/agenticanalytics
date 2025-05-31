0c19a76bb6cbc0c03e741ff9cd0228c2c2df6f70
ahnopologetic <stahn1995@gmail.com>
2025-05-31 15:24:41 -0400

    refactor: remove unused

--- ui/web/src/components/ui/GitHubConnect.tsx
+++ ui/web/src/components/ui/GitHubConnect.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { getGithubRepos, getUserSessionList, saveGithubToken, talkToAgent } from '../../api'
+import { getGithubRepos, saveGithubToken, talkToAgent } from '../../api'
 import { useUserContext } from '../../hooks/use-user-context'
 import { supabase } from '../../supabaseClient'
 import ConnectGitHubStep from './github-connect/ConnectGitHubStep'
